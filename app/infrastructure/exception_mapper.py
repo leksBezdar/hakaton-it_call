adf877c07455800c919cfaa6376378548fcab6ee
leks <vollkovalex1991@mail.ru>
2024-07-03 23:57:22 +0400

    Minor changes

--- app/infrastructure/exception_mapper.py
+++ app/infrastructure/exception_mapper.py
@@ -19,7 +19,6 @@ def exception_mapper(
         try:
             return await func(*args, **kwargs)
         except SQLAlchemyError as err:
-            raise err
             raise RepositoryException from err
 
     return wrapped
--- app/infrastructure/services/codes/redis.py
+++ app/infrastructure/services/codes/redis.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
-import random
+from random import randint
+from typing import Union
 
 from domain.entities.users import UserEntity
 from domain.exceptions.verification_tokens import (
@@ -14,22 +15,26 @@ class RedisCodeService(ICodeService, IRedisClient):
     # TODO replace redis with aioredis (redis.asyncio)
     def generate_code(self, user: UserEntity) -> str:
         # TODO replace random.randint code with a generated, user-binded token
-        code = str(random.randint(10**5, 10**6 - 1))
-        self.redis_client.set(user.email.as_generic_type(), code)
+        user_email = user.email.as_generic_type()
+        code = str(randint(10**5, 10**6 - 1))
+        self.redis_client.set(user_email, code)
 
         return code
 
     def validate(self, code: str, user: UserEntity) -> None:
-        cached_code: bytes = self.redis_client.get(user.email.as_generic_type())
         # TODO move exception handling to command layer
+        user_email = user.email.as_generic_type()
+        cached_code: Union[bytes, None] = self.redis_client.get(user_email)
+
         if cached_code is None:
             raise CodeWasNotFoundException(code=code)
 
-        if cached_code.decode("utf-8") != code:
+        decoded_cached_code = cached_code.decode("utf-8")
+        if decoded_cached_code != code:
             raise CodesAreNotEqualException(
                 code=code,
-                cached_code=cached_code.decode("utf-8"),
-                user_email=user.email.as_generic_type(),
+                cached_code=decoded_cached_code,
+                user_email=user_email,
             )
 
-        self.redis_client.delete(user.email.as_generic_type())
+        self.redis_client.delete(user_email)
